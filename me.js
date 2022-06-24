0c8a2f2fee45e6f683ae01ded363a4ef3e5a4e06
Olusanmi Pamilerin kehinde <105246664+Erinayo0000@users.noreply.github.com>
2022-06-24 06:20:24 +0100

    Add files via upload
    first commit

---
+++ me.js
@@ -0,0 +1,12 @@
+var nameString = function(name) {return "Hello , I'm" + " " + name;}
+var name = nameString ("Olusanmi Pamilerin kehinde")
+nameString("Olusanmi Pamilerin kehinde"); alert(name)
+
+var heightString = function(height) {return "i'm" + " " + height + " "+ "tall";}
+var height= heightString ("5ft 3 inches")
+heightString ("150cm"); alert(height)
+
+
+var countryString = function( country) {return "i'm from"+ " "+ country;}
+var country =countryString (" i'm Nigeria")
+countryString ("Nigeria"); alert(country)
